a92ba5d779286fadd2c2da57e015cf3e9e9bec93
unknown <minhquang2705@gmail.com>
2017-12-05 13:45:26 -0500

    update

---
+++ keys.py
@@ -0,0 +1,2 @@
+keychain ={"youtube":{"client_id":"126920892433-e58m7pn87600743drvlsjsanlcsus2av.apps.googleusercontent.com","project_id":"project5-187402","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"flFfvozgUappY1MIRCPE5Cxc","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"],"scope":["https://www.googleapis.com/auth/youtube.readonly"]}}
+
